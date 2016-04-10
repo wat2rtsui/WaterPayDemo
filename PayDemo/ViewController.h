7d8c5bd8079bb1ab40626ce00179024d154bdd1d
Water Tsui <appwater@icloud.com>
2016-04-10 09:50:05 +0800

    Initial Commit

---
+++ PayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayDemo
+//
+//  Created by Water on 10/4/16.
+//  Copyright © 2016年 Water. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PayDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PayDemo
+//
+//  Created by Water on 10/4/16.
+//  Copyright © 2016年 Water. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
