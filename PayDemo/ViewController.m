9afbc536f6594021da0576cc7749c35c7c69dd1e
wat2rtsui <water@Waters-MacBook-Air.local>
2016-04-10 11:05:34 +0800

    效果預覽

--- PayDemo/ViewController.m
+++ PayDemo/ViewController.m
@@ -7,16 +7,24 @@
 //
 
 #import "ViewController.h"
+#import <LocalAuthentication/LocalAuthentication.h>
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
-
+-(void)viewWillAppear:(BOOL)animated{
+    @autoreleasepool {
+        [self Author];
+    }
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    @autoreleasepool {
+        [self Author];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +32,30 @@
     // Dispose of any resources that can be recreated.
 }
 
-@end
+
+- (IBAction)TouchToVerify:(id)sender {
+    [self Author];
+}
+-(void)viewDidDisappear:(BOOL)animated{
+    for(UIView *view in self.view.subviews){[view removeFromSuperview];}
+    [self.view removeFromSuperview];
+    NSLog(@"exit");
+}
+
+-(void)Author{
+    @autoreleasepool {
+        LAContext  *authenticationContext= [[LAContext alloc]init];
+        NSError *error;
+        if ([authenticationContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+            [authenticationContext evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"輕觸TouchID驗證身份" reply:^(BOOL success, NSError * _Nullable error) {
+                if (success) {
+                    [self performSegueWithIdentifier:@"ToMain" sender:nil];
+
+                }
+            }];
+        }else{
+
+        }
+    }
+}
+    @end
